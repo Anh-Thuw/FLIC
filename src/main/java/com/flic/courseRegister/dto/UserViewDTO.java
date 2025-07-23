439a60ed828bbab4a9e4ddc20cbeb5264accbb89
hadd24667 <hahnj24667@gmail.com>
2025-07-24 00:37:26 +0700

    them chuc nang tao user va huong dan luong code

---
+++ src/main/java/com/flic/courseRegister/dto/UserViewDTO.java
@@ -0,0 +1,20 @@
+package com.flic.courseRegister.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserViewDTO {
+    private Long id;
+    private String email;
+    private String fullName;
+    private String avatarUrl;
+    private String role;
+    private String status;
+}
+

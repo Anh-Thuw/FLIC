b53d1bdb4ba99f138b01e40079421658bb8e32cc
longthien123 <longthienl80@gmail.com>
2025-08-22 16:29:55 +0700

    sua lai input LocalDateTime theo dang dd/MM/yyyy HH:mm

--- src/main/java/com/flic/courseRegister/dto/lecture/LessonUpdateDTO.java
+++ src/main/java/com/flic/courseRegister/dto/lecture/LessonUpdateDTO.java
@@ -1,9 +1,11 @@
 package com.flic.courseRegister.dto.lecture;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -13,8 +15,12 @@ import java.time.LocalDateTime;
 @Builder
 public class LessonUpdateDTO {
    private String title;
-   private String description;
+   private String description;g
    private Integer weekIndex ;
+   @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
    private LocalDateTime plannedAt;
+   @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
    private LocalDateTime endTime;
 }

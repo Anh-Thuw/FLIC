a373b8da2d7280d4a43984942b4b4fe27c04244a
longthien123 <longthienl80@gmail.com>
2025-10-11 15:42:34 +0700

    setAllowed

--- src/main/java/com/flic/courseRegister/security/SecurityConfig.java
+++ src/main/java/com/flic/courseRegister/security/SecurityConfig.java
@@ -41,7 +41,10 @@ public class    SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // Cho phép frontend từ domain cụ thể gọi API
-        config.setAllowedOrigins(List.of("http://localhost:5173/")); // team FE sau này thay ip fe vào để dùng API
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",   // vẫn cho phép local dev
+                "https://flic-frontend-production.up.railway.app/" // domain FE sau deploy
+        )); // team FE sau này thay ip fe vào để dùng API
 
         // Cho phép các HTTP method
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
